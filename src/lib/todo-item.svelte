73886ba24176dd883325cd98ea2b3752beae4d2e
Lucas Lo <lucas.lmlok@gmail.com>
2022-01-07 16:42:12 +0000

    ADD PATCH endpoint - mark as done

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     export let todo: Todo;
+    const { done } = todo;
 </script>
 
 <style>
@@ -24,6 +25,7 @@
         background-color: transparent;
         background-position: 50% 50%;
         background-repeat: no-repeat;
+        cursor: pointer;
     }
 
     .todo input {
@@ -50,6 +52,7 @@
         position: absolute;
         right: 0;
         opacity: 0;
+        cursor: default !important;
         background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 2H3.5C2.67158 2 2 2.67157 2 3.5V20.5C2 21.3284 2.67158 22 3.5 22H20.5C21.3284 22 22 21.3284 22 20.5V3.5C22 2.67157 21.3284 2 20.5 2Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M17 2V11H7.5V2H17Z' fill='white' stroke='white' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M13.5 5.5V7.5' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M5.99844 2H18.4992' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3C/svg%3E%0A");
     }
 
@@ -57,6 +60,7 @@
     .save:focus {
         transition: opacity 0.2s;
         opacity: 1;
+        cursor: pointer !important;
     }
 
     .delete {
@@ -81,10 +85,10 @@
     }
 </style>
 
-<div class="todo">
-    <form action="" method="">
-        <input type="hidden" name="done" value="">
-        <button aria-label="Mark done/not done" class="toggle"></button>
+<div class="todo" class:done>
+    <form action="/todos/{todo.uid}.json?_method=patch" method="POST">
+        <input type="hidden" name="done" value="{done ? '' : 'true'}">
+        <button aria-label="Mark todo as {done ? 'not done' : 'done'}" class="toggle"></button>
     </form>
 
     <form action="/todos/{todo.uid}.json?_method=patch" method="POST" class="text">
